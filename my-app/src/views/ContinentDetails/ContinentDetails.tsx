8cc6711586889c31b48222476e6c6b39399539b5
KrystianKozlowski111 <54074452+KrystianKozlowski111@users.noreply.github.com>
2021-07-04 19:47:50 +0200

    Continent Details init

--- my-app/src/views/ContinentDetails/ContinentDetails.tsx
+++ my-app/src/views/ContinentDetails/ContinentDetails.tsx
@@ -1,39 +1,53 @@
 import React, { useState } from 'react';
 import { gql, useQuery, useLazyQuery } from '@apollo/client';
-
+import { useParams } from 'react-router-dom';
+interface RouteParams {
+    code: string
+}
 
 interface ContinentsList {
   code: string;
   name: string;
+  emoji:string;
 
 }
 
 interface ContinentsData {
   continents: ContinentsList[];
 }
-
+interface ContinentsListVars {
+    code: string;
+  }
 
 
 const LIST_CONTINENTS = gql`
-  {
-    continent(code:"AF") {
-        name
+query continent($code: ID!) {
+    continent(code: $code) {
         countries{
-          name
-        }       
-      }
+        name
+        emoji
+        languages{
+            name
+        }
+    }
+    }
   }
 `;
+type Props = {
+    code: string;
+}
 
-function Continent() {
 
-  const { loading, data } = useQuery<ContinentsData>(
-    LIST_CONTINENTS
-  );
+function ContinentDetails() {
+    const params = useParams<RouteParams>();
 
+  const { loading, data } = useQuery<ContinentsData, ContinentsListVars>(
+    LIST_CONTINENTS,
+    { variables: { code: params.code } }
+
+  );  
     
-    
-    console.log('data ' + JSON.stringify(data))
+    console.log('data '+JSON.stringify(data)+'  code   '+params.code)
     return (
 
         <>
@@ -43,4 +57,4 @@ function Continent() {
     );
 }
 
-export default Continent;
+export default ContinentDetails;
